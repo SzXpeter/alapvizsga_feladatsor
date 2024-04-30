0ba6f1bd3db9e79ad863d31e08cec51c0fc0210a
Szabó Péter <szabo.peter@students.jedlik.eu>
2024-04-30 11:34:07 +0200

    1. python feladat

---
+++ python/1f.py
@@ -0,0 +1,14 @@
+"""
+1.0936 yard
+39.370 hüvelyk
+3.2808 láb
+0,000621 mérföld
+"""
+
+hossz = float(input("Adja meg a MÉTER-ek számát: "))
+
+print(f"{hossz} méter:")
+print(f"\t{(hossz * 1.0936):.4f} yard")
+print(f"\t{(hossz * 39.370):.4f} hüvelyk")
+print(f"\t{(hossz * 3.2808):.4f} láb")
+print(f"\t{(hossz * 0.000621):.6f} mérföld")
