e7349902b965acf4bacbaba653d2c9ed71b81d65
Szabó Péter <szabo.peter@students.jedlik.eu>
2024-05-13 13:53:18 +0200

    2. feladat kiegészítés; 3. feladat befelyezés python_vizsga docx elkezdése

--- python/2f.py
+++ python/2f.py
@@ -17,22 +17,25 @@ def bekeres():
         else:
             return tipp
 
+def tipp_mix(szam):
+    while i < 3:
+        i = 0
+        tipp = bekeres()
+        if szam > tipp:
+            print(f"A szám nagyobb mint {tipp}.")
+        elif szam < tipp:
+            print(f"A szám kisebb mint {tipp}.")
+        else:
+            break
+        i += 1
+    return i
+
 def main():
     while True:
         szam = random.randint(1, 10)
         print(f"{szam = }")
-        i = 0
-        while i < 3:
-            tipp = bekeres()
-            if szam > tipp:
-                print(f"A szám nagyobb mint {tipp}.")
-            elif szam < tipp:
-                print(f"A szám kisebb mint {tipp}.")
-            else:
-                break
-            i += 1
-        
-        if i < 3:
+
+        if tipp_mix(szam) < 3:
             print("Gratulálok nyert.")
         else:
             print("Sajnálom vesztett.")
@@ -42,6 +45,5 @@ def main():
             vege = input("Szeretne mégegyet játszan: Igen(I)/Nem(N)")
         if vege == "N":
             break
-
 
 main()
--- python/3f.py
+++ python/3f.py
@@ -9,16 +9,27 @@ bekérdezés->kiadók játékai
 class Game:
     def __init__(self, row):
         data = row.strip().split(';')
-        nev = data[1]
-        eladasok = data[2]
-        szeria = data[3]
-        platform = data[4]
-        megjelenes = data[5]
-        fejleszto = data[6]
-        kiado = data[7]
+        self.nev = data[0]
+        self.eladasok = data[1]
+        self.szeria = data[2]
+        self.platform = data[3]
+        self.megjelenes = data[4]
+        self.fejleszto = data[5]
+        self.kiado = data[6]
 
 games: list[Game] = []
 
 file = open("bestsellers.csv", 'r', encoding="UTF-8")
 for row in file:
     games.append(Game(row))
+
+print(f"2. feladat: A fájlban {len(games) - 1} játék adata szerepel.")
+
+print("\n3. feladat: játékok-fejlesztők")
+for game in games:
+    print(f"{game.nev} -> {game.fejleszto}")
+
+kiado = input("\n4. feladat: Kiadó megadása: ")
+for game in games:
+    if game.kiado.lower() ==  kiado.lower():
+        print(game.nev)
